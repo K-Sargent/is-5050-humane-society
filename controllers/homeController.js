3a6048957e1c54ba7ec94448fbd8edbc56c73836
k-sargent <kate.sargent424@gmail.com>
2021-10-05 14:22:50 -0600

    updated top nav, created some routes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,27 @@
 exports.resHome = (req, res) => {
 	res.render("index");
 };
+exports.resPetlist = (req, res) => {
+	res.render("petlist");
+};
+exports.resAbout = (req, res) => {
+	res.render("about");
+};
+exports.resAccount = (req, res) => {
+	res.render("account");
+};
+exports.resAddEvent = (req, res) => {
+	res.render("add-event");
+};
+exports.resContactUs = (req, res) => {
+	res.render("contact-us");
+};
+exports.resDiscussions = (req, res) => {
+	res.render("discussions");
+};
+exports.resDonate = (req, res) => {
+	res.render("donate");
+};
+exports.resEvents = (req, res) => {
+	res.render("events");
+};
--- main.js
+++ main.js
@@ -10,6 +10,15 @@ app.use(layouts);
 app.use(express.static("public"));
 
 app.get("/", homeController.resHome);
+app.get("/index", homeController.resHome);
+app.get("/petlist", homeController.resPetlist);
+app.get("/about", homeController.resAbout);
+app.get("/account", homeController.resAccount);
+app.get("/add-event", homeController.resAddEvent);
+app.get("/contact-us", homeController.resContactUs);
+app.get("/discussions", homeController.resDiscussions);
+app.get("/donate", homeController.resDonate);
+app.get("/events", homeController.resEvents);
 
 app.use(errorController.logErrors);
 app.use(errorController.respondNoResourceFound);
